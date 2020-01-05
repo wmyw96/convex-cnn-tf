0411a1bfebd7e12c1fce962f66f922ef1fe38c0e
Yihong Gu <wmyw96@126.com>
2020-01-06 02:42:57 +0800

    add nnh

--- comp_graph.py
+++ comp_graph.py
@@ -289,5 +289,103 @@ def build_grafting_onecut_model(params):
 
 
 
+def build_neural_network_hybrid_model(params):
+    # build placeholder
+
+    inp_x = tf.placeholder(dtype=tf.float32, 
+                           shape=[None] + params['data']['x_size'],
+                           name='x')
+    label = tf.placeholder(dtype=tf.int64,
+                           shape=[None],
+                           name='label')
+    is_training = tf.placeholder(dtype=tf.bool, shape=[], name='is_training')
+    lr_decay = tf.placeholder(dtype=tf.float32, shape=[], name='lr_decay')
+
+    ph = {
+        'x': inp_x,
+        'y': label,
+        'is_training': is_training,
+        'lr_decay': lr_decay
+    }
+
+    # build computation graph
+    nclass = params['data']['nclass']
+    model_name = params['network']['model']    # vgg16, vgg16-large, vgg19 
+    use_bn = params['network']['batch_norm']
+    num_nets = params['hybrid']['num_nets']
+    domains = ['hybrid'] +  ['net' + str(k) for k in range(num_nets)]
+
+    modules = {}
+    graph = {
+        'one_hot_y': tf.one_hot(label, nclass)
+    }
+    for domain in domains:
+        modules[domain] = \
+            make_layers_vgg_net(scope=domain,
+                                input_x=inp_x, 
+                                config=config[model_name],
+                                num_classes=nclass,
+                                dropout_rate=params['network']['dropout'],
+                                is_training=is_training,
+                                batch_norm=use_bn,
+                                layer_mask=None)
+        graph[domain] = modules[domain]
+
+    net_vars = {}
+    save_vars = []
+    # fetch variables
+    for domain in domains:
+        net_vars[domain] = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 
+            scope=domain)
+        show_params(domain, net_vars[domain])
+        save_vars += net_vars[domain]
+
+    targets = {}
+
+    nets_domain = ['net' + str(k) for k in range(num_nets)]
+    # num_nets default-training domain
+    for domain in nets_domain:
+        # build tragets
+        logits = modules[domain]['out']
+        ce_loss = tf.nn.softmax_cross_entropy_with_logits(labels=graph['one_hot_y'], 
+            logits=logits, dim=1)
+        ce_loss = tf.reduce_mean(ce_loss)
+        acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(logits, 1), ph['y']), tf.float32))   # [1,]
+
+        regularizer = get_regularizer_loss(net_vars[domain], params['network']['regularizer'])
+
+        loss = ce_loss + regularizer * params['network']['regw']
+    
+        sl_op = tf.train.MomentumOptimizer(params['train']['lr'] * ph['lr_decay'], 0.9, use_nesterov=True)
+        sl_grads = sl_op.compute_gradients(loss=loss, var_list=net_vars[domain])
+        sl_train_op = sl_op.apply_gradients(grads_and_vars=sl_grads)
+        sl_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope=domain)
+    
+        show_grads(domain, sl_grads)
+    
+        train = {
+            'train': sl_train_op,
+            'update': sl_update_ops,
+            'overall_loss': loss,
+            'ce_loss': ce_loss,
+            'reg_loss': regularizer,
+            'acc_loss': acc
+        }
+        #for gd, var in sl_grads:
+        #    train[var.name + '_gd_loss'] = tf.reduce_mean(tf.abs(gd)) - 5e-4 * tf.reduce_mean(tf.abs(var))
+    
+        test = {
+            'overall_loss': loss,
+            'ce_loss': ce_loss,
+            'reg_loss': regularizer,
+            'acc_loss': acc
+        }
+
+        targets[domain] = {
+            'train': train,
+            'eval': test
+        }
+
+    return ph, graph, save_vars, net_vars, targets
 
 
